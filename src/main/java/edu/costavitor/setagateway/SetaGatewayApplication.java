a09d53223919ad8db6b0966f325c51789ab7c909
deltavitor <cvitor1618@gmail.com>
2024-08-24 19:09:38 -0300

    Initial commit

---
+++ src/main/java/edu/costavitor/setagateway/SetaGatewayApplication.java
@@ -0,0 +1,13 @@
+package edu.costavitor.setagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetaGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/costavitor/setagateway/SetaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.costavitor.setagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
